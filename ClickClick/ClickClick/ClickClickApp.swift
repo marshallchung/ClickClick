278b4a96edd4d5a9f2a0d6eda6633ffb3765c5fd
marshallchung <mr811208@gmail.com>
2026-02-25 17:28:02 +0800

    20260225_1728

---
+++ ClickClick/ClickClick/ClickClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickClickApp.swift
+//  ClickClick
+//
+//  Created by Marshall Chung on 2026/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ClickClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
